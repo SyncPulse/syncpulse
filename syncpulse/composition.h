cfaf4aac0f8fd89cbc83075bb7ad9770c52ddb39
Simon.Y Yoon <simon.y@hpcnt.com>
2023-09-13 19:36:00 +0900

    include license comment header

--- syncpulse/composition.h
+++ syncpulse/composition.h
@@ -1,3 +1,12 @@
+/**
+ * Copyright 2023, SyncPulse
+ *     Author: Yoon SeungYong <forcom@forcom.kr>
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ **/
+
 #ifndef __COMPOSITION_H__
 #define __COMPOSITION_H__
 #include <gst/gst.h>
